52b06dcd23e6a5d7a7714e00bf52c05802950962
Jaime <mrjaime1999@gmail.com>
2018-06-30 01:31:34 +0200

    Created packages

--- Client/com/example/chatapp/client/Connection.java
+++ Client/com/example/chatapp/client/Connection.java
@@ -1,3 +1,4 @@
+package com.example.chatapp.client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- Server/com/example/chatapp/server/Connection.java
+++ Server/com/example/chatapp/server/Connection.java
@@ -1,3 +1,4 @@
+package com.example.chatapp.server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
